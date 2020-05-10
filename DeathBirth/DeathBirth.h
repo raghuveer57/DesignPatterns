eda202abe51d340482dc5021b81cc8a52a6ca71c
Raghuveer Ch S <raghuveerchs@gmail.com>
2020-05-10 19:04:05 +0530

    Code clean up and hello world pogram for factor method

--- DeathBirth/DeathBirth.h
+++
@@ -1,8 +0,0 @@
-﻿// MiscProblems.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
